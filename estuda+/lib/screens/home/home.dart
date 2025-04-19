c7912d51f06e9052f68e1d6f5aac359bb9c46fdf
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-04-19 06:37:15 -0300

    ajustes

--- estuda+/lib/screens/home/home.dart
+++ estuda+/lib/screens/home/home.dart
@@ -68,135 +68,131 @@ class _HomeScreenState extends State<HomeScreen> {
         drawer: Drawer(
             backgroundColor: const Color.fromARGB(255, 209, 209, 209),
             child: MenuDrawer()),
-        body: Stack(
-          children: <Widget>[
-            Background(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListView(
-                  children: [
-                    // WIDGET BOXRESUM DAS QUANTIDAS DE QUESTÕES RESPODIDAS CORRETAMENTE
-                    BoxResum(
-                      valueProvider.correctsCurrents,
-                      'Corretas',
-                      Lottie.asset('./assets/lotties/Animation_correct.json'),
-                      TextButton(
-                        onPressed: () {
-                          if (valueProvider
-                              .resultQuestionsCorrects.isNotEmpty) {
-                            Routes().pushRoute(
-                                context, const AccumulatedCorrects());
-                            //Limpa a lista que guarda os assuntos selecionados.
-                            valueProvider.subjectsAndSchoolYearSelected.clear();
-                            //Fecha onde mostra os assuntos selecionados.
-                            valueProvider.showSubjects(false);
-                          } else {
-                            showSnackBarError(
-                              context,
-                              'Não temos nenhuma questão correta.',
-                              Colors.blue,
-                            );
-                          }
-                        },
-                        child: Text(
-                          'Resumo',
-                          style: AppTheme.customTextStyle(
-                              fontSize: 13,
-                              color: Colors.black,
-                              underline: true),
-                        ),
-                      ),
-                    ),
-                    // DASHBORD DAS DISCIPLINAS RESPONDIDAS CORRETAMENTE
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount:
-                          valueProvider.listDisciplinesAnsweredCorrects.length,
-                      itemBuilder: (context, int index) {
-                        return DashbordDisplice(
-                          valueProvider.listDisciplinesAnsweredCorrects[index]
-                              ['discipline'],
-                          Colors.green,
-                          valueProvider.listDisciplinesAnsweredCorrects[index]
-                                  ['amount'] /
-                              100,
-                          valueProvider.listDisciplinesAnsweredCorrects[index]
-                                  ['amount']
-                              .toString(),
+        body: Background(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                // WIDGET BOXRESUM DAS QUANTIDAS DE QUESTÕES RESPODIDAS CORRETAMENTE
+                BoxResum(
+                  valueProvider.correctsCurrents,
+                  'Corretas',
+                  Lottie.asset('./assets/lotties/Animation_correct.json'),
+                  TextButton(
+                    onPressed: () {
+                      if (valueProvider
+                          .resultQuestionsCorrects.isNotEmpty) {
+                        Routes().pushRoute(
+                            context, const AccumulatedCorrects());
+                        //Limpa a lista que guarda os assuntos selecionados.
+                        valueProvider.subjectsAndSchoolYearSelected.clear();
+                        //Fecha onde mostra os assuntos selecionados.
+                        valueProvider.showSubjects(false);
+                      } else {
+                        showSnackBarError(
+                          context,
+                          'Não temos nenhuma questão correta.',
+                          Colors.blue,
                         );
-                      },
+                      }
+                    },
+                    child: Text(
+                      'Resumo',
+                      style: AppTheme.customTextStyle(
+                          fontSize: 13,
+                          color: Colors.black,
+                          underline: true),
                     ),
-                    // WIDGET BOXRESUM DAS QUANTIDAS DE QUESTÕES RESPODIDAS INCORRETAMENTE
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: BoxResum(
-                        valueProvider.incorrectsCurrents,
-                        'Incorretas',
-                        Lottie.asset('./assets/lotties/alert.json'),
-                        TextButton(
-                          onPressed: () {
-                            if (valueProvider
-                                .resultQuestionsIncorrects.isNotEmpty) {
-                              routes.pushRoute(
-                                context,
-                                const AccumulatedIncorrects(),
-                              );
-                              //Limpa a lista que guarda os assuntos selecionados.
-                              valueProvider.subjectsAndSchoolYearSelected
-                                  .clear();
-                              //Fecha onde mostra os assuntos selecionados.
-                              valueProvider.showSubjects(false);
-                            } else {
-                              showSnackBarError(
-                                context,
-                                'Não temos nenhuma questão incorreta.',
-                                Colors.blue,
-                              );
-                            }
-                          },
-                          child: Text(
-                            'Resumo',
-                            style: AppTheme.customTextStyle(
-                                fontSize: 13,
-                                color: Colors.black,
-                                underline: true),
-                          ),
-                        ),
-                      ),
-                    ),
-                    // WIDGET DASHBORD DISCIPLINAS RESPONDIDAS INCORRETAMENTE
-                    ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: valueProvider
-                            .listDisciplinesAnsweredIncorrects.length,
-                        itemBuilder: (context, int index) {
-                          return DashbordDisplice(
-                              valueProvider
-                                      .listDisciplinesAnsweredIncorrects[index]
-                                  ['discipline'],
-                              Colors.red,
-                              valueProvider.listDisciplinesAnsweredIncorrects[
-                                      index]['amount'] /
-                                  100,
-                              valueProvider
-                                  .listDisciplinesAnsweredIncorrects[index]
-                                      ['amount']
-                                  .toString());
-                        }),
+                  ),
+                ),
+                // DASHBORD DAS DISCIPLINAS RESPONDIDAS CORRETAMENTE
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount:
+                      valueProvider.listDisciplinesAnsweredCorrects.length,
+                  itemBuilder: (context, int index) {
+                    return DashbordDisplice(
+                      valueProvider.listDisciplinesAnsweredCorrects[index]
+                          ['discipline'],
+                      Colors.green,
+                      valueProvider.listDisciplinesAnsweredCorrects[index]
+                              ['amount'] /
+                          100,
+                      valueProvider.listDisciplinesAnsweredCorrects[index]
+                              ['amount']
+                          .toString(),
+                    );
+                  },
+                ),
+                // WIDGET BOXRESUM DAS QUANTIDAS DE QUESTÕES RESPODIDAS INCORRETAMENTE
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: BoxResum(
+                    valueProvider.incorrectsCurrents,
+                    'Incorretas',
+                    Lottie.asset('./assets/lotties/alert.json'),
                     TextButton(
                       onPressed: () {
-                        routes.pushRoute(context, const ScreenInitial());
+                        if (valueProvider
+                            .resultQuestionsIncorrects.isNotEmpty) {
+                          routes.pushRoute(
+                            context,
+                            const AccumulatedIncorrects(),
+                          );
+                          //Limpa a lista que guarda os assuntos selecionados.
+                          valueProvider.subjectsAndSchoolYearSelected
+                              .clear();
+                          //Fecha onde mostra os assuntos selecionados.
+                          valueProvider.showSubjects(false);
+                        } else {
+                          showSnackBarError(
+                            context,
+                            'Não temos nenhuma questão incorreta.',
+                            Colors.blue,
+                          );
+                        }
                       },
-                      child: const Text(
-                        'Sair',
-                        style: TextStyle(color: Colors.white),
+                      child: Text(
+                        'Resumo',
+                        style: AppTheme.customTextStyle(
+                            fontSize: 13,
+                            color: Colors.black,
+                            underline: true),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                // WIDGET DASHBORD DISCIPLINAS RESPONDIDAS INCORRETAMENTE
+                ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: valueProvider
+                        .listDisciplinesAnsweredIncorrects.length,
+                    itemBuilder: (context, int index) {
+                      return DashbordDisplice(
+                          valueProvider
+                                  .listDisciplinesAnsweredIncorrects[index]
+                              ['discipline'],
+                          Colors.red,
+                          valueProvider.listDisciplinesAnsweredIncorrects[
+                                  index]['amount'] /
+                              100,
+                          valueProvider
+                              .listDisciplinesAnsweredIncorrects[index]
+                                  ['amount']
+                              .toString());
+                    }),
+                TextButton(
+                  onPressed: () {
+                    routes.pushRoute(context, const ScreenInitial());
+                  },
+                  child: const Text(
+                    'Sair',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: GestureDetector(
