d402daa5601373308076705d0946ecc73d497eec
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-06-26 22:23:01 -0300

    Update dashboard_usuario.dart

--- blurt/blurt/lib/screens/dashboard_usuario.dart
+++ blurt/blurt/lib/screens/dashboard_usuario.dart
@@ -5,7 +5,6 @@ import 'package:blurt/features/autenticacao/presentation/controllers/login_contr
 import 'package:blurt/provider/provider_controller.dart';
 import 'package:blurt/models/profissional/profissional.dart';
 import 'package:blurt/theme/themes.dart';
-import 'package:blurt/widgets/pageview_pre_analise.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_animate/flutter_animate.dart';
